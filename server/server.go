d78bcbea3223dd66900c04f9d93af864dd766a77
Jordan McArdle <jordanmcardle@gmail.com>
2019-05-23 01:57:12 -0500

    Initial commit - May 23, 2019.

---
+++ server/server.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"github.com/satori/go.uuid"
+	"log"
+	"net"
+	"strconv"
+)
+
+type StreamServer struct {
+	Port     int
+	GameList []GameInfo
+}
+
+type GameInfo struct {
+	Title string
+	Path  string
+}
+
+var PlayerList []*Player
+
+// Run Running the cloud gaming server
+func (server *StreamServer) Run() {
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(server.Port))
+	if err != nil {
+		log.Fatal("Error listening", err.Error())
+	}
+	log.Println("Listen port:", server.Port)
+
+	// Set the first player to None
+
+	NonePlayer := new(Player)
+	NonePlayer.ID = "None"
+	PlayerList = append(PlayerList, NonePlayer)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting", err.Error())
+			return
+		}
+
+		// Generate unique ID for each player
+		//PlayerID, _ := uuid.NewV4()
+		PlayerID := uuid.NewV4()
+		player := &Player{
+			Conn:     conn,
+			ID:       PlayerID.String(),
+			GameList: &server.GameList,
+		}
+
+		PlayerList = append(PlayerList, player)
+
+		if player.InitTelnet() {
+			go player.Serve()
+		}
+	}
+}
